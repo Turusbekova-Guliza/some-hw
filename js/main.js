9af13743f28700894b84fa6bd55f66f826187079
Turusbekova-Guliza <134058547+Turusbekova-Guliza@users.noreply.github.com>
2023-07-08 19:12:00 +0600

    some

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@
 //         }
 //     })
 // }
- 
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector(".header").classList.toggle("open")
